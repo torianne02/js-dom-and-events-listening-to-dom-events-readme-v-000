55b9666325d87c35cc400aae3e73cd27c2698314
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-27 00:37:08 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -39,3 +39,16 @@ for (let i = 0; i < divs.length; i++) {
   // set the third argument to `true`!
   divs[i].addEventListener('click', capture, true);
 }
+
+const divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
